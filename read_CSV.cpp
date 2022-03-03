ea318e292f7b4380100d83a5583c74a84958f83b
Nazmus-1225 <76961621+Nazmus-1225@users.noreply.github.com>
2022-03-03 11:43:52 +0600

    Update read_CSV.cpp

--- read_CSV.cpp
+++ read_CSV.cpp
@@ -1,26 +1,61 @@
 #include<fstream>
 #include<stdlib.h>
 #include<iostream>
+#include<sstream>
 #include<string>
+#include<string.h>
+#include<cstring>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     ifstream fp;
-    fp.open("Book1.csv");
+    char x[]="Book1.csv";
+    fp.open(x);
     string line="";
-    int i,n=1;
+    int i,j,n=1,m=0;
     getline(fp, line);
     for(i=0;i<line.length();i++){
         if(line.at(i)==','){n++;}
     }
-    cout<<"Number of variables:"<<n;
-    getline(fp, line,',');
-    cout<<"Roll: "<<line<<endl;
-    getline(fp, line,',');
-    cout<<"Phone: "<<line<<endl;
-    float x;
-    fp>>x;
-    cout<<"CGPA: "<<x;
+    cout<<"Number of variables:"<<n<<endl;
+    fp.close();
+    fp.open(x);
+    for(i=0;;++i)
+        {
+            getline(fp,line,'\n');
+            if(!fp)
+            break;
+            m++;
+            }
+    fp.close();
+
+    cout<<"Number of records:"<<m<<endl;
+
+    float a[m][n];
+
+    fp.open(x);
+    for(i=0;i<m;i++){
+        getline(fp,line,'\n');
+        stringstream X(line);
+        for(j=0;j<n;j++){
+            string temp;
+            getline(X,temp,',');
+            a[i][j]=atof(temp.c_str());}
+
+        if(!fp)
+            break;
+
+    }
+
+
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cout<<a[i][j]<<"\t";
+    }
+        cout<<"\n";}
+
     return 0;
 
 }
+
